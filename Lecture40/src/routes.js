b99e8b3b5a336adbca1f72174360069c5c85d4c6
DAVID RUIZ DE LA ROSA <david.ruizdelarosa@gmail.com>
2017-01-20 15:20:24 +0100

    Nested Routes

---
+++ Lecture40/src/routes.js
@@ -0,0 +1,36 @@
+(function(){
+  'use strict';
+  angular.module('ShoppingList')
+
+  .config(RoutesConfig);
+
+  RoutesConfig.$inject=['$stateProvider','$urlRouterProvider'];
+  function RoutesConfig($stateProvider,$urlRouterProvider){
+
+    // Redirect to home page if no others URL matches
+    $urlRouterProvider.otherwise('/');
+
+    //Home page
+    $stateProvider
+    .state('home',{
+      url:'/',
+      templateUrl:"src/shoppinglist/templates/home.template.html"
+    })
+    .state('mainList',{
+      url:"/main-list",
+      templateUrl:"src/shoppinglist/templates/main-shopping.template.html",
+      controller:"MainShoppingListController as mainList",
+      resolve:{
+        items: ['ShoppingListService',function(ShoppingListService){
+          return ShoppingListService.getItems();
+        }]
+      }
+    })
+    .state('mainList.itemDetail',{
+      url:'/item-detail/{itemId}',
+      templateUrl:'src/shoppinglist/templates/item_detail.template.html',
+      controller:'ItemDetailController as itemDetail'
+    });
+  };
+
+})();
---
+++ Lecture40/src/shoppinglist/itemdetail.controller.js
@@ -0,0 +1,16 @@
+(function(){
+  'use strict';
+
+  angular.module('ShoppingList')
+  .controller('ItemDetailController',ItemDetailController);
+
+  //'item' is injected through state's resolve
+  ItemDetailController.$inject=['$stateParams','items']
+  function ItemDetailController($stateParams,items){
+    var itemDetail=this;
+    var item = items[$stateParams.itemId];
+    itemDetail.name=item.name;
+    itemDetail.quantity=itemDetail.quantity;
+    itemDetail.description=itemDetail.description;
+  }
+})();
