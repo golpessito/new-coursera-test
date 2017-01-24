ad6ce797f2d337c9b15e07b3bd743700e2b591ab
DAVID RUIZ DE LA ROSA <david.ruizdelarosa@gmail.com>
2017-01-24 15:12:58 +0100

    Unit Test Controller Version2

---
+++ Lecture44/spec/shoppinglist.controller.spec-v2.js
@@ -0,0 +1,37 @@
+describe('Spec V2: ShoppingListController',function(){
+
+  beforeEach(function(){
+    module(function($provide){
+      $provide.service("ShoppingListServiceErrorMock",function(){
+        var service=this;
+
+        service.addItem=function(name, quantity){
+          throw new Error("Test Message.");
+        };
+
+        service.getItems=function(){
+          return null;
+        };
+      });
+
+    });
+
+    module('ControllerAsAPP');
+  });
+
+  var $controller;
+  var shoppingListController;
+
+  beforeEach(inject(function(_$controller_,ShoppingListServiceErrorMock){
+    $controller=_$controller_;
+
+    shoppingListController=$controller('ShoppingListController',
+                  {ShoppingListService:ShoppingListServiceErrorMock});
+  }));
+
+  it("should change error message in controller",function(){
+    shoppingListController.addItem();
+    expect(shoppingListController.error).toBe("Test Message.");
+  });
+
+});
